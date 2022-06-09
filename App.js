8895c410921b95b097d548f0e0fb08a6ec252b00
rmdhnz3 <milhamramadhan998@gmail.com>
2022-06-09 10:21:35 +0700

    first

--- App.js
+++ App.js
@@ -1,20 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { render } from 'react-dom';
 import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import CalculatorScreen from './screens/CalculatorScreem';
+export default class App extends React.Component{
+ render(){
+  return <CalculatorScreen/>
+ };
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
